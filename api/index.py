dc1730503a026a1117f009e6bf6f53babf4869f8
souridas <53730297+souridas@users.noreply.github.com>
2023-06-24 07:01:20 -0700

    Update index.py

--- api/index.py
+++ api/index.py
@@ -7,21 +7,14 @@ def handler(request):
     # Extract the input data from the request
     input_data = data.get("Please enter your github url: ")
 
-    # Perform any necessary data processing or validation
-    repositories= 
     # Invoke your existing Python code using the input data
-    result = your_function(input_data)
+    repositories= fetch_user_repo(input_data)
+    result = (repositories)
 
     # Return the result as the HTTP response
     response = {
         "statusCode": 200,
         "body": result
     }
-    """### User input"""
-
-
-
-
-most_complex(repositories)
 
     return response
